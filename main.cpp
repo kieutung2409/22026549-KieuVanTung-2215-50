3e054ba97efc5272f34c83c22a048bdccc660d96
kieutung2409 <kieutung242004@gmail.com>
2024-07-15 22:57:24 +0700

    update background

--- main.cpp
+++ main.cpp
@@ -1,81 +1,58 @@
-#include <SDL2/SDL.h>
+#include <SDL2/SDL.h>  
+#include <stdio.h>
 
-#define W_WIDTH     800
-#define W_HEIGHT    600
+int WinMain(int argc, char* argv[]) {  
+    SDL_Window* window = NULL;  
+    SDL_Renderer* renderer = NULL;  
 
-#define BLACK       0, 0, 0, 255
-#define WHITE       255, 255, 255, 255
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {  
+        printf("SDL initialization failed: %s\n", SDL_GetError());  
+        return 1;  
+    }  
 
-SDL_bool        game_runs = SDL_TRUE;
+    window = SDL_CreateWindow("Pong Game", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1200, 600, 0);  
+    if (window == NULL) {  
+        printf("Error creating window: %s\n", SDL_GetError());  
+        return 1;  
+    }  
 
-SDL_Window      *p_Window = NULL;
-SDL_Renderer    *p_Render = NULL;
-SDL_Event       event;
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);  
+    if (renderer == NULL) {  
+        printf("Error creating renderer: %s\n", SDL_GetError());  
+        return 1;  
+    }  
 
-void game_init(void);
-void game_inputs(void);
-void game_clean(void);
+    // Load and display background image  
+    SDL_Surface* bg_surface = SDL_LoadBMP("back.bmp");  
+    if (bg_surface == NULL) {  
+        printf("Error loading background image: %s\n", SDL_GetError());  
+        return 1;  
+    }  
 
-int main(int argc, char **argv)
-{
-    (void)argc;
-    (void)argv;
+    SDL_Texture* bg_texture = SDL_CreateTextureFromSurface(renderer, bg_surface);  
+    SDL_RenderCopy(renderer, bg_texture, NULL, NULL);  
+    SDL_RenderPresent(renderer);  
 
-    game_init();
+    // Main game loop  
+    bool quit = false;  
+    while (!quit) {  
+        // Game logic and rendering  
 
-    while(game_runs)
-    {
-        game_inputs();
+        // Handle events  
+        SDL_Event event;  
+        while (SDL_PollEvent(&event)) {  
+            if (event.type == SDL_QUIT) {  
+                quit = true;  
+            }  
+        }  
+    }  
 
-        SDL_SetRenderDrawColor(p_Render, BLACK);
-        SDL_RenderClear(p_Render);
+    // Clean up resources  
+    SDL_FreeSurface(bg_surface);  
+    SDL_DestroyTexture(bg_texture);  
+    SDL_DestroyRenderer(renderer);  
+    SDL_DestroyWindow(window);  
+    SDL_Quit();  
 
-        SDL_RenderPresent(p_Render);
-
-        SDL_Delay(16);
-    }
-
-    game_clean();
-    return 0;
-}
-
-void game_init(void)
-{
-    if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-        SDL_Log("Error SDL Init: %s\n", SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
-
-    p_Window = SDL_CreateWindow("Test_Pong", 
-                                SDL_WINDOWPOS_CENTERED,
-                                SDL_WINDOWPOS_CENTERED,
-                                W_WIDTH, W_HEIGHT,
-                                SDL_WINDOW_SHOWN);
-    p_Render = SDL_CreateRenderer(p_Window, -1, SDL_RENDERER_ACCELERATED |
-                                                SDL_RENDERER_PRESENTVSYNC);
-
-    if(!p_Window && !p_Render) {
-        SDL_Log("Error SDL window and renderer: %s\n", SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
-}
-
-void game_inputs(void)
-{
-    while(SDL_PollEvent(&event)) {
-        if(SDL_QUIT == event.type) game_runs = SDL_FALSE;
-        if(SDL_KEYDOWN == event.type) {
-            switch(event.key.keysym.sym) {
-                case SDLK_ESCAPE: game_runs = SDL_FALSE;
-                    break;
-            }
-        }
-    }
-}
-
-void game_clean(void)
-{
-    SDL_DestroyRenderer(p_Render);
-    SDL_DestroyWindow(p_Window);
-    SDL_Quit();
+    return 0;  
 }
